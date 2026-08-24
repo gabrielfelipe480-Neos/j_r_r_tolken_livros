d9e79687875944f35767c73787b37e5478a45ced
gabrielfelipe480-Neos <gabrielfelipeoliveira480@gmail.com>
2026-08-24 19:27:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const titulo = document.querySelector("h1");
+const botaoSobre = document.querySelector("#botaoSobre");
+
+botaoSobre.addEventListener("click", function () {
+
+    titulo.textContent = "Bem-vindo, amante da fantasia!";
+
+});
